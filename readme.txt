f43944cb8dc320d6c2ebd4033eee3521f88840b6
Andy Fragen <andy@thefragens.com>
2012-08-08 22:18:29 -0700

    fixes for deactivate code

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: afragen
 Tags: headers, rotate headers
 Requires at least: 3.4
 Tested up to: 3.4.1
-Stable tag: 0.5.6
+Stable tag: 0.5.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -23,7 +23,7 @@ Create a new page. It can be private. It must be titled `The Headers`. Add any i
 == Changelog ==
 
 = 1.0 =
-* Initial commit
+* Initial commit to WordPress repository
 
 == Current Version ==
 
@@ -31,4 +31,4 @@ Using https://github.com/jkudish/WordPress-GitHub-Plugin-Updater
 
 The line below is used for the updater API, please leave it untouched unless bumping the version up :)
 
-~Current Version:0.5.6~
+~Current Version:0.5.7~
--- rotate-custom-headers.php
+++ rotate-custom-headers.php
@@ -4,7 +4,7 @@
 Plugin Name: Rotate Custom Headers
 Plugin URI: https://github.com/afragen/rotate-custom-headers
 Description: Remove default headers and add custom headers. Images must be added to new page titled 'The Headers'.  Idea and code from <a href="http://juliobiason.net/2011/10/25/twentyeleven-with-easy-rotating-header-images/">Julio Biason</a>.
-Version: 0.5.6
+Version: 0.5.7
 Author: Andy Fragen
 Author URI: http://thefragens.com/blog/
 License: GNU General Public License v2
@@ -21,10 +21,9 @@ function rch_headers_page_present () {
 		$pageTitles[] = get_the_title($pageID);
 	}
 	if ( ( !in_array('The Headers', $pageTitles) ) || ( !($wp_version >= 3.4) ) ) {
-		echo '<div class="error">
-       <p>Rotate Custom Headers requires a page titled 'The Headers' with images and WordPress v3.4 or greater.</p>
+		echo '<div class="error"><p>Rotate Custom Headers requires a page titled \'The Headers\' with images and WordPress v3.4 or greater.</p>
     </div>';
-		deactivate_plugins( $plugin );
+		deactivate_plugins(__FILE__);
 	}
 }
 //print_r($pageTitles);
