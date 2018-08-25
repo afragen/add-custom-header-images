63ce23785453d6b66377da32d2bbae4b29e89cc7
Poul Hornsleth <poulh@umich.edu>
2018-08-25 15:32:02 -0400

    Fix incorrect URL if IP changes after img uploaded
    previously image url was $image->guid, however this can be incorrect if the IP address of site changes. using wp_get_attachment_url is 'smart' and takes IP changes into consideration.

--- add-custom-header-images.php
+++ add-custom-header-images.php
@@ -116,7 +116,7 @@ class Add_Custom_Header_Images {
 			$thumb = wp_get_attachment_image_src( $image->ID, 'medium' );
 
 			$headers[] = array(
-				'url'           => $image->guid,
+				'url'           => wp_get_attachment_url($image->ID),
 				'thumbnail_url' => $thumb[0],
 				'description'   => $image->post_title,
 				'attachment_id' => $image->ID,
